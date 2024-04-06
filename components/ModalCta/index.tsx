19c3b248da9b6f7d6411d6f8036f6c25a432c2d2
animico <animico@gmail.com>
2024-04-05 21:49:46 -0700

    Stylings con tailwind y ajusted generales

--- components/ModalCta/index.tsx
+++ components/ModalCta/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 const ModalCta = ({ title, children, color }: Props) => {
   const [showForm, setShowForm] = useState(false);
   return <>
-    <button onClick={() => setShowForm(true)} className={`rounded-full ${color ? color : 'text-white bg-black'} px-8 py-4 font-bold`}>{title}</button>
+    <button onClick={() => setShowForm(true)} className={`rounded-full ${color ? color : 'text-white bg-black'} px-8 py-4`}>{title}</button>
     <div className={`fixed top-0 left-0 right-0 bottom-0 backdrop-blur-xl bg-white/30 ${showForm ? 'opacity-100' : 'opacity-0 pointer-events-none'} transitions-all duration-300`}>
       <div className="fixed bg-white text-black rounded-3xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2">
         <button className=" absolute font-3xl right-10 top-10" onClick={() => setShowForm(false)}>&times;</button>
@@ -18,5 +18,6 @@ const ModalCta = ({ title, children, color }: Props) => {
     </div>
   </>
 }
+
 
 export default ModalCta;
