fc3873d66a12fd1d0ec265c736e7052a408e66d8
alex-romo <alex@nebulosastudio.com>
2024-04-09 11:59:30 -0700

    fix: removed _document and added viewport to meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Branding Pool",
   description: "BRANDS WITH A FUTURE",
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
 export default function RootLayout({
