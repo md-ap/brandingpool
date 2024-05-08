7aaabd9463d6306ee58c3fc65141b0ace903b114
alex-romo <alex@nebulosastudio.com>
2024-05-08 13:53:37 -0700

    feat: updated emails

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: NextRequest) {
 
     const mailOptions: Mail.Options = {
         from: process.env.MY_EMAIL,
-        to: process.env.MY_EMAIL,
+        to: ['hola@somospool.com', 'contigo@somospool.com'],
         // cc: email, (uncomment this line if you want to send a copy to the sender)
         subject: `Message from ${name} (${email})`,
         text: `
